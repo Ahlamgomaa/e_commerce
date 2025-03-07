bf9cafb091d9e02839050863c29f2e1959680286
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-03-07 14:11:31 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main (){
+  runApp(ecommerceAPP());
+}
+class ecommerceAPP extends StatelessWidget{
+  const ecommerceAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp();
+  }
+
+}
---
+++ lib/view/screens/onboarding.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class onBoarding extends StatelessWidget{
+  
+}
